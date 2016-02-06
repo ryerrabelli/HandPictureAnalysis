ba19c50fa2f20e5a7f892268f2fb4e52d7929c90
Random Guy <Random Guy>
2016-02-06 11:26:22 -0500

    The data set + the open cv file

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import numpy as np                       #importing libraries
 
 
 def blob(filename):
-    img = cv2.imread('images/'+str(filename)+".jpg",1)
-    '''img = cv2.rectangle(im,(384,0),(510,128),(0,255,0),3)
+    im = cv2.imread('images/'+str(filename)+".jpg",1)
+    img = cv2.rectangle(im,(384,0),(510,128),(0,255,0),3)
     imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(imgray,127,255,0)
     im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -12,14 +12,14 @@ def blob(filename):
     cv2.drawContours(im2, contours, -1, (0,255,0), 3)
     cv2.imshow('contours',img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()'''
-    crop_img = img[200:400, 100:300] # Crop from x, y, w, h -> 100, 200, 300, 400
+    cv2.destroyAllWindows()
+    '''crop_img = img[200:400, 100:300] # Crop from x, y, w, h -> 100, 200, 300, 400
     # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
     cv2.imshow("cropped", crop_img)
-    cv2.waitKey(0)
+    cv2.waitKey(0)'''
 
 cap = cv2.VideoCapture(0)                #creating camera object
-filename = 0
+filename = 178
 is10 = 0
 while( cap.isOpened() ) :
     ret,frame = cap.read()
@@ -27,8 +27,9 @@ while( cap.isOpened() ) :
     blur = cv2.GaussianBlur(gray,(5,5),0)
     '''ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_MASK)
     cv2.imshow('OTSU',thresh1)                  #displaying the frames'''
-    ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    cv2.imshow('255',thresh1)   #good for dark bacground
+    ret,thresh2 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    thresh1 = thresh2[thresh2.shape[0]/3:thresh2.shape[0],thresh2.shape[0]/8:thresh2.shape[1]/8*7]
+    cv2.imshow('256',thresh1)   #good for dark bacground
     if (is10  == 10) :
         filePath = 'images/' + str(filename)
         cv2.imwrite(filePath+".jpg",thresh1)
@@ -39,14 +40,17 @@ while( cap.isOpened() ) :
             for y in range(thresh1.shape[1]) :
                 number = thresh1[x][y];
                 if (number == 255) :
-                    file.write("1 ")
+                    file.write("1,")
                 else :
-                    file.write("0 ");
-            file.write("\n"); 
+                    file.write("0,");
+            file.write("\n");
             
     is10 += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
+    if cv2.waitKey(1)& 0xFF == ord('p'):
+        cv2.waitKey(0)
 cap.release()
 cv2.destroyAllWindows()
+#blob(12)
