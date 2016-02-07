675a3e0590ddf01200c174834d19d97629c6349f
Random Guy <Random Guy>
2016-02-07 04:13:40 -0500

    Redid test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import cv2                              
+import cv2
+from whichcluster import whichcluster
 import numpy as np                       #importing libraries
 
 
@@ -29,8 +30,23 @@ while( cap.isOpened() ) :
     cv2.imshow('OTSU',thresh1)                  #displaying the frames'''
     ret,thresh2 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     thresh1 = thresh2[thresh2.shape[0]/3:thresh2.shape[0],thresh2.shape[0]/8:thresh2.shape[1]/8*7]
+    result = whichcluster(thresh1)
+    if (result == 1):
+        print "1"
+    if (result == 2) :
+        print "2"
+    if (result == 3):
+        print "3"
+    if (result == 4) :
+        print "4"
+    if (result == 5):
+        print "5"
+    if (result == 6) :
+        print "6"
+    if (result == 7):
+        print "7"
     cv2.imshow('256',thresh1)   #good for dark bacground
-    if (is10  == 10) :
+    '''if (is10  == 10) :
         filePath = 'images/' + str(filename)
         cv2.imwrite(filePath+".jpg",thresh1)
         filename += 1
@@ -43,7 +59,7 @@ while( cap.isOpened() ) :
                     file.write("1,")
                 else :
                     file.write("0,");
-            file.write("\n");
+            file.write("\n");'''
             
     is10 += 1
 
