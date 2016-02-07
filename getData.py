b8f29dcc58b69f566a42a1611b3400c546cb6c46
Random Guy <Random Guy>
2016-02-07 07:18:19 -0500

    works

--- getData.py
+++ getData.py
@@ -19,9 +19,9 @@ def blob(filename):
     # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
     cv2.imshow("cropped", crop_img)
     cv2.waitKey(0)'''
-motion = -1
+
 cap = cv2.VideoCapture(0)                #creating camera object
-filename = 178
+filename = 76
 is10 = 0
 while( cap.isOpened() ) :
     ret,frame = cap.read()
@@ -31,29 +31,8 @@ while( cap.isOpened() ) :
     cv2.imshow('OTSU',thresh1)                  #displaying the frames'''
     ret,thresh2 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     thresh1 = thresh2[thresh2.shape[0]/3:thresh2.shape[0],thresh2.shape[0]/8:thresh2.shape[1]/8*7]
-    cv2.imwrite("input.jpg",thresh1)    
-    array =convertsmall("input.jpg")
-    list1=[]
-    for i in range(array.shape[0]):
-        list1.extend(array[i])
-    result=whichcluster(list1)
-    if (result == 1):
-        motion = 1
-    if (result == 2) :
-        motion = 2    
-    if (result == 3):
-        motion = 3
-    if (result == 4) :
-        motion = 4
-    if (result == 5):
-        motion= 5
-    if (result == 6) :
-        motion = 6
-    if (result == 7):
-        motion = 7
     cv2.imshow('256',thresh1)   #good for dark bacground
-    print motion
-    '''if (is10  == 10) :
+    if (is10  == 10) :
         filePath = 'images/' + str(filename)
         cv2.imwrite(filePath+".jpg",thresh1)
         filename += 1
@@ -66,7 +45,7 @@ while( cap.isOpened() ) :
                     file.write("1,")
                 else :
                     file.write("0,");
-            file.write("\n");'''
+            file.write("\n");
             
     is10 += 1
 
